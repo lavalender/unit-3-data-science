dede083e1b84d235a723a1f2979ee61e4eaf541f
Valerie Burke <vburke@brooklynsteamcenter.org>
2025-12-09 10:50:28 -0500

    website

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+import pymysql
+
+from dynaconf import Dynaconf
+
+
+app = Flask(__name__)
+
+config = Dynaconf(settings_file=["settings.toml"])
+
+def connect_db():
+    conn = pymysql.connect(
+        host="db.steamcenter.tech",
+        user="cogboe",
+        password=config.password,
+        database="cogboe_heekaudio",
+        autocommit= True,
+        cursoorclass= pymysql.cursors.DictCursor
+    )
+    return conn
+
+
+@app.route("/")
+def index():
+    return render_template("homepage.html.jinja")
+
+@app.route("/browse")
+def browse():
+    connection = connect_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM `Product`")
+    result = cursor.fetchall()
+    connection.close()
+    return render_template("browse.html.jinja")
+
